6e425f35409d0c209e699f97aeb30fbabd8a0522
melquiadesmario <melquiadesm@gmail.com>
2020-08-11 01:01:11 -0300

    Tests Passed: 100%

--- src/Palindromo.java
+++ src/Palindromo.java
@@ -1,19 +1,10 @@
 class Palindromo {
-    
-    public String[] palindromo = new String[] {"ovo", "OvO", "radar", "aba"};
 
     boolean ehPalindromo(String palavra) {
-        //throw new UnsupportedOperationException("Not supported yet.");
-        if(palavra == palindromo[0]){
-            return true;
-        }else if(palavra == palindromo[1]){
-            return true;
-        }else if(palavra == palindromo[2]){
-            return true;
-        }else if(palavra == palindromo[3]){
+        String palindromo = new StringBuffer(palavra).reverse().toString();
+        if(palavra.equals(palindromo)){
             return true;
         }
-        
         return false;
     }
     
